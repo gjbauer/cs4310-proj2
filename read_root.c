5cf51fa08ee699d9f439e0fd47c5c009973327ed
Gabriel Bauer <gabeb1277@gmail.com>
2025-05-01 11:28:32 -0400

    back to the beginning

--- read_root.c
+++ read_root.c
@@ -5,6 +5,8 @@
 #include "bitmap.h"
 #include "directory.h"
 
+dirent stop;
+
 int readdir()
 {
     int rv;
@@ -19,14 +21,62 @@ int readdir()
     	rv = printf("%s\n", e0->name);
     	if (!strcmp(e1->name, "*")) break;
     	rv = printf("%s\n", e0->name);
-    	n = get_inode(n->iptr);
+    	if (n->iptr != 0) n = get_inode(n->iptr);
+    	else return 0;
     }
+}
+
+int
+mknod(const char *path)
+{
+    int rv = 0;
+    int count = 0;
+    int l = alloc_inode(path);
+    inode *n = get_inode(1);
+    inode *h = get_inode(l);
+    dirent *p0, *p1;
+    dirent data;
+    data.inum=l;
+    strcpy(data.name, path);
+    //n->mode=mode;
+    data.active=true;
+mk_loop:
+	p0 = (dirent*)((char*)get_root_start()+n->ptrs[0]);
+	p1 = (dirent*)((char*)get_root_start()+n->ptrs[1]);
+	if (!strcmp(p0->name, "")) {
+		memcpy(p0, &data, sizeof(data));
+		h->ptrs[0] = n->ptrs[0];
+	} else if (!strcmp(p0->name, "*")) {
+		strcpy(stop.name, "*");
+		memcpy(p0, &data, sizeof(data));
+		h->ptrs[0] = n->ptrs[0];
+		memcpy(p1, &stop, sizeof(stop));
+	} else if (!strcmp(p1->name, "")) {
+		memcpy(p1, &data, sizeof(data));
+		h->ptrs[1] = n->ptrs[1];
+	} else if (!strcmp(p1->name, "*")) {
+		strcpy(stop.name, "*");
+		memcpy(p0, &data, sizeof(data));
+		h->ptrs[0] = n->ptrs[1];
+		if (n->iptr==0) n->iptr = alloc_inode("");
+		n = get_inode(n->iptr);
+		p0 = (dirent*)((char*)get_root_start()+n->ptrs[0]);
+		//p1 = (dirent*)((char*)get_root_start()+n->ptrs[1]);
+		memcpy(p0, &stop, sizeof(stop));
+	} else {
+		n = get_inode(n->iptr);
+		goto mk_loop;
+	}
+    printf("mknod(%s) -> %d\n", path, rv);
+    return rv;
 }
 
 int
 main(int argc, char *argv[])
 {
 	storage_init("data.nufs");
+	readdir();
+	mknod("/hello.txt");
 	readdir();
 	pages_free();
 }
