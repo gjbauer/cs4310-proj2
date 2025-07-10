a493d84886d4de544d6a50c2e769411fea0dd807
Gabriel Bauer <gabeb1277@gmail.com>
2025-07-10 04:30:11 -0400

    goodnight

--- directory.c
+++ directory.c
@@ -17,18 +17,14 @@ int tree_lookup(const char* path) {
 	
 	printf("Level of file in directory structure: %d\n", level);
 	
-	memcpy((char*)&file, get_root_start(), sizeof(dirent));
-	
-	printf("Dirent name: %s\n", file.name);
-	
 	ptr = root;
 	
 	for (int i=0; i<level; i++) {
 		memcpy((char*)&file, get_data(ptr->ptrs[i%2]), sizeof(dirent));
-		printf("current search target: %s\n", split(path, i));
-		printf("current file: %s\n", file.name);
-		printf("inum: %d\n", ptr->inum);
-		printf("i % 2: %d\n", i%2);
+		//printf("current search target: %s\n", split(path, i));
+		//printf("current file: %s\n", file.name);
+		//printf("inum: %d\n", ptr->inum);
+		//printf("i % 2: %d\n", i%2);
 		if (!strcmp(file.name, split(path, i))) {
 			dirent *temp = (dirent*)get_data(ptr->ptrs[i%2]);
 			
