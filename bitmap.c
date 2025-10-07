5729d72f395ec6b0e93ab9317956085abe5a9ab6
Gabriel Bauer <gabeb1277@gmail.com>
2025-10-07 16:20:14 -0400

    .

--- bitmap.c
+++ bitmap.c
@@ -1,15 +1,15 @@
 #include "bitmap.h"
-
+#include <stdlib.h>
 int bitmap_get(void* bm, int ii) {
 	uint64_t* ptr = (uint64_t*)bm;
 	ptr = ptr + ( ii / 64 );
-	return (*ptr & (1 << (ii % 64))) >> (ii % 64);
+	return (*ptr & ((uint64_t)1 << (ii % 64))) >> (ii % 64);
 }
 
 void bitmap_put(void* bm, int ii, int vv) {
 	uint64_t* ptr = (uint64_t*)bm;
 	ptr = ptr + ( ii / 64 );
-	*ptr = (vv==0) ? *ptr & ~(1 << (ii % 64)) : *ptr | (1 << (ii % 64));
+	*ptr = (vv==0) ? *ptr & ~((uint64_t)1 << (ii % 64)) : *ptr | ((uint64_t)1 << (ii % 64));
 }
 
 void bitmap_print(void* bm, int size) {
--- many-files.sh
+++ many-files.sh
@@ -4,5 +4,10 @@ mkdir mnt/nested
 
 for n in {1..50}; 
 do
-    touch "mnt/nested/"$n".nums"
+    echo "$n" > "mnt/nested/"$n".nums"
+done
+
+for n in {1..5}; 
+do
+    cat "mnt/nested/"$((n*10))".nums"
 done
